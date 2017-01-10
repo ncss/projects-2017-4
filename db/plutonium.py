efc01b0439561210dc16c0d9b63f68317610e3ea
Robert Fraser <fluffyxvi@gmail.com>
2017-01-10 12:22:25 +1100

    Add more stub functions and update comments
    Improved comments, added missing comments, and added the remaining
    functions that are required before the minimum product.

--- db/plutonium.py
+++ db/plutonium.py
@@ -28,11 +28,32 @@ class User:
         print('Obtained user.')
         return User()
         
+    def get_all():
+        '''
+        Returns a list of all the users
+        NOTE: This can be large - be careful
+        '''
+        print( 'All users.' )
+        
     def edit_displayname( self, newname ):
         '''
         Changes the displayname of a user class.
         '''
         self.displayname = newname
+        print('Display name updated.')
+        
+    def get_posts( self ):
+        '''
+        Returns a list of all the post objects that the user has made
+        '''
+        print('Post objects.')
+        
+    def rate( self, post, rating ):
+        '''
+        Gets the user to vote on a given post object
+        Rating should be -1, 0, or 1
+        '''
+        print( 'Vote cast!' )
         
     # TODO: Functions for verified status and title management
         
@@ -45,17 +66,48 @@ class Post:
         self.location = 'James\' Secret Headquarters'
         
     def create( user, title, description, image, location ):
+        '''
+        Creates a new post given the user object, title, description, image, and location.
+        '''
         print('New post created!')
         return Post()
         
     def get( postid ):
+        '''
+        Returns a post object given a postid
+        '''
         print('Obtained post!')
         return Post()
     
+    def get_all():
+        '''
+        Returns a list containing every post object
+        NOTE: This can be large - be careful
+        '''
+        print('All the posts!')
+        
+    def get_by_recent( amount ):
+        '''
+        Returns some of the most recent posts created
+        '''
+        print( 'Most recent posts' )
+        
+    def get_by_location( location, amount ):
+        '''
+        Returns some of the nearest posts
+        '''
+        print( 'Nearby posts' )
+    
     def rating( self ):
+        '''
+        Returns the rating of a post.
+        '''
         print('Return ratings.')
         
     def comments( self ):
+        '''
+        Returns a list of all the comments on the post
+        '''
         print('Return comments.')
 
 class Comment:
@@ -66,5 +118,9 @@ class Comment:
         print('Incredible new comment!')
         
     def create(user, postid, contents ):
+        '''
+        Given a user object, a post object, and a string
+        creates a new comment on the specified post
+        '''
         print('New comment has been created!')
         return Comment()
