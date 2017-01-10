7b90ab6ea058ec8102f1af7af0f1c54be30fdff4
Maddie Wagner <musomaddy@gmail.com>
2017-01-11 10:40:55 +1100

    Fixed up the create db page
    Our changes were removed after merge so I quickly redid them.

--- db/create_db.py
+++ db/create_db.py
@@ -7,14 +7,13 @@ cur = conn.cursor()
 cur.execute('''
 
 CREATE TABLE user (
-    user_id INTEGER NOT NULL,
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     password TEXT NOT NULL,
     email TEXT NOT NULL,
     username TEXT NOT NULL,
     levels INTEGER NOT NULL,
     is_verified BOOLEAN NOT NULL,
-    profile_picture TEXT NOT NULL,
-    PRIMARY KEY (user_id)
+    profile_picture TEXT NOT NULL
     );
 ''')
 
@@ -22,14 +21,13 @@ cur.execute('''
 
 CREATE TABLE post (
 
-    post_id INTEGER NOT NULL,
-    author INTEGER NOT NULL,
+    post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
     location STRING NOT NULL,
     title STRING NOT NULL,
     description STRING NOT NULL,
     image STRING NOT NULL,
     rating INTEGER NOT NULL,
-    PRIMARY KEY (post_id),
     FOREIGN KEY (author_id) REFERENCES user (user_id)
     );
 ''')
@@ -37,38 +35,27 @@ CREATE TABLE post (
 cur.execute('''
 
 CREATE TABLE comments (
-    comment_id INTEGER NOT NULL,
+    comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id INTEGER NOT NULL,
     author INTEGER NOT NULL,
     comment STRING NOT NULL,
-    PRIMARY KEY (comment_id),
     FOREIGN KEY (post_id) REFERENCES post (post_id),
     FOREIGN KEY (author) REFERENCES post (user_id)
     );
-''')    
+''')
 
 cur.execute('''
 
 CREATE TABLE post_ratings (
-    rating_id INTEGER NOT NULL,
+    rating_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user INTEGER NOT NULL,
     post INTEGER  NOT NULL,
     rating INTEGER NOT NULL,
-    PRIMARY KEY (rating_id),
     FOREIGN KEY (user) REFERENCES user (user_id),
     FOREIGN KEY (post) REFERENCES post (post_id)
     );
-''')      
-    
+''')
 
-    
- 
-while True:
-    row = cur.fetchone()
-    if not row:
-        break
-    print(row)
-    
 conn.commit()
-    
+
 conn.close()
