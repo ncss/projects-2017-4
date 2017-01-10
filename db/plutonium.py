4e5d04c33a364b09d8335bfc8177424f613f7196
Robert Fraser <fluffyxvi@gmail.com>
2017-01-10 12:30:18 +1100

    Minor updates to User class
    User class can now be created more dynamically, as expected. Functions
    currently return a placeholder James Curran account.

--- db/plutonium.py
+++ db/plutonium.py
@@ -2,31 +2,35 @@ class User:
     '''
     User Object
     '''
-    def __init__(self):
-        self.email = 'james.r.curran@sydney.edu.au'
-        self.displayname = 'James Curran'
+    def __init__(self, email, displayname, displaypicture, verified):
+        self.email = email
+        self.displayname = displayname
+        self.displaypicture = displaypicture
+        self.verifed = verified
         
     def login( email, password ):
         '''
         Given a email address and a password, will return the user class or False depending
         on whether or not the login was successful.
+        
+        Currently will only return James Curran
         '''
-        print( 'User has been logged in.' )
-        return User()
+        print( 'Logging in as Supreme Overlord James Curran' )
+        return User( 'james.r.curran@sydney.edu.au', 'James Curran', 'jamescurran.png', True )
     
     def register( email, password, displayname ):
         '''
         Registers a new user given an email address, password, and display name.
         '''
         print( 'New user registered.' )
-        return User()
+        return User( 'james.r.curran@sydney.edu.au', 'James Curran', 'jamescurran.png', True )
         
     def get( email ):
         '''
         Gets a user object given an email.
         '''
         print( 'Obtained user.' )
-        return User()
+        return User( 'james.r.curran@sydney.edu.au', 'James Curran', 'jamescurran.png', True )
         
     def get_all():
         '''
