622cfc0864d97320de8259bfaa13b374e2321fb9
Robert Fraser <fluffyxvi@gmail.com>
2017-01-12 00:58:18 +1100

    Added test case for post creation
    Intensive testing for new features which have been added

--- db/plutonium_test.py
+++ db/plutonium_test.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from plutonium import database_connect
 from plutonium import terminate_connection
-from plutonium import User
+from plutonium import User, Post, Comment, Ratings
 from create_db import init_db
 
 class TestPlutonium(unittest.TestCase):
@@ -34,6 +34,12 @@ class TestPlutonium(unittest.TestCase):
         User.register( 'james.r.curran@sydney.edu.au', 'tomtom', 'James Curran' )
         u = User.get( 'james.r.curran@sydney.edu.au' )
         self.assertEqual( u.username, 'James Curran' )
+		
+    def test_post(self):
+        u = User.register( 'james.r.curran@sydney.edu.au', 'tomtom', 'James Curran' )
+        p = Post.create( u.user_id, 'Title', 'Content', 'Image', 'Location' )
+        self.assertEqual( u.user_id, p.author_id )
+			
 
     def tearDown(self):
         terminate_connection()
