5aca1401a3c6216a2d3a02e736be97b542fe42fd
Damian Camilleri <contact@damiancamilleri.com>
2017-01-11 14:54:48 +1100

    Added rating to post initialiser and filled post stubs
    filled create, get and get_all stubs

--- db/plutonium_damion.py
+++ db/plutonium_damion.py
@@ -24,110 +24,16 @@ def hash_password( email, password ):
     h.update( string.encode() )
     return h.hexdigest()
 
-class User:
-    '''
-    User Object
-    '''
-    def __init__(self, user_id, email, username, level, is_verified, profile_picture ):
-        self.user_id = user_id
-        self.email = email
-        self.username = username
-        self.level = level
-        self.is_verifed = is_verified
-        self.profile_picture = profile_picture
-
-    @staticmethod
-    def login( email, password ):
-        '''
-        Given an email address and an unhashed password
-        Will return a User object if valid login
-        Will throw error if invalid login
-        '''
-        c = conn.cursor()
-        password = hash_password( email, password )
-        # Ensure that the user is in the database
-        c.execute('SELECT * FROM user WHERE email = ?;', (email,) )
-        data = c.fetchone()
-        if data is None:
-            raise ValueError("User is not in database")
-        else:
-            storedhash = data[1]
-            # Hash the given password and compare it to the storedhash
-            if password == storedhash:
-                return User( data[0], data[2], data[3], data[4], data[5], data[6] )
-            else:
-                raise ValueError("Password Mismatch")
-
-    @staticmethod
-    def register( email, password, username ):
-        '''
-        Given the required data, registers an account in the database
-        Will return False if the account does not meet registration requirements
-        '''
-        c = conn.cursor()
-        # Check that the email is not currently in the database
-        c.execute('SELECT email FROM user WHERE email = ?;', (email,) )
-        if len( c.fetchall() ) > 0:
-            return False
-        else:
-            password = hash_password( email, password )
-            c.execute('INSERT INTO user (password, email, username, levels, is_verified, profile_picture) VALUES(?, ?, ?, ?, ?, ?);', (password, email, username, 0, 0, 'default.png') )
-            conn.commit()
-            return User( user_id, email, username, 0, 0, 'default.png' )
-
-    @staticmethod
-    def get( email ):
-        '''
-        Gets a user object given an email.
-        '''
-        c = conn.cursor()
-        c.execute('SELECT * FROM user WHERE email = ?;', (email,) )
-        data = c.fetchone()
-        if data is None:
-            raise ValueError("User is not in database")
-        else:
-            return User( data[0], data[2], data[3], data[4], data[5], data[6] )
-
-    def get_all():
-        '''
-        Returns a list of all the users
-        NOTE: This can be large - be careful
-        '''
-        print( 'All users.' )
-
-    def edit_displayname( self, newname ):
-        '''
-        Changes the displayname of a user class.
-        '''
-        self.displayname = newname
-        print( 'Display name updated.' )
-
-    def get_posts( self ):
-        '''
-        Returns a list of all the post objects that the user has made
-        '''
-        print( 'Post objects.' )
-
-    def rate( self, post, rating ):
-        '''
-        Gets the user to vote on a given post object
-        Rating should be -1, 0, or 1
-        '''
-        print( 'Vote cast!' )
-
-    # TODO: Functions for verified status and title management
-
-User.register('rfras399@walrusfamily.com', 'alphabetical', 'Robert Fraser' )
-
 class Post:
-    def __init__(self, post_id, author_id, location, title, description, image):
+    def __init__(self, post_id, author_id, location, title, description, image, rating):
         self.id = post_id
         self.author_id = author_id
         self.location = location
         self.title = title
         self.description = description
         self.image = image
-        self.rating = Rating.post_rating(post_id)
+
+        #self.rating = Rating.post_rating(post_id) #TODO: UNCOMMENT THIS!!!
 
     def create( user, title, description, image, location ):
         '''
@@ -149,24 +55,51 @@ class Post:
         title,
         description,
         image,
-        000
+        0 #TODO: Calculate from ratings table
         ))
-        print( 'New post created!' )
-        return Post()
+        conn.commit()
+        print( '[Post.create]' )
+        post_id = cur.lastrowid
+        rating = 0 # Initial value of the rating column
+        return Post(post_id, user, location, title, description, image, rating)
 
     def get( postid ):
         '''
         Returns a post object given a postid
         '''
-        print( 'Obtained post!' )
-        return Post()
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT *
+        FROM post
+        WHERE post_id = ?
+        """, (
+        postid,
+        ))
+        response = cur.fetchone()
+        print( '[Post.get] post_id:', postid ) #self, post_id, author_id, location, title, description, image
+        return Post(postid, response[1], response[2], response[3], response[4], response[5], response[6])
 
     def get_all():
         '''
         Returns a list containing every post object
         NOTE: This can be large - be careful
         '''
-        print( 'All the posts!' )
+
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT *
+        FROM post
+        ORDER BY post_id DESC
+        """)
+
+        postsArr = []
+
+        for row in cur:
+            postsArr.append(Post(row[0], row[1], row[2], row[3], row[4], row[5], row[6])) #self, post_id, author_id, location, title, description, image
+
+        print( '[Post.get_all]' )
+
+        return postsArr
 
     def get_by_recent( amount ):
         '''
